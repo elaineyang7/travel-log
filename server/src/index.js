e0668337ae4f43ebf8f7fa7ca4e032f1eca7993e
elaineapp082020 <joyfulelaine7@gmail.com>
2021-01-17 14:34:53 -0800

    first commit

---
+++ server/src/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const app = express();
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors({
+  orgin: 'http://localhost:3000',
+}));
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World!',
+  });
+});
+
+app.use((req, res, next) => {
+  const error = new Error(`Not Fount - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+});
+
+// eslint-disable-next-line no-unused-vars 
+app.use((error, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: error message,
+    stack: process.env.NODE_ENV === 'production' ? '' : error.stack,
+  });
+});
+
+const port = process.env.port || 1337;
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
+
